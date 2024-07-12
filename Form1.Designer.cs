deeae6465b9ec2b1be08c57b5df93fdb06882084
GempaDeveloperTeam <133745625+GempaDeveloperTeam@users.noreply.github.com>
2024-07-12 15:28:59 +0300

    res. errors has been fixed.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -13,8 +13,6 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -26,7 +24,7 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.Size = new System.Drawing.Size(1920, 1080);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -35,17 +33,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Mandelbrot Set Visualizer";
+            this.Text = "Mandelbrot Set Viewer";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-
         }
-
-        #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
     }
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,9 @@ using System.Windows.Forms;
 using ILGPU;
 using ILGPU.Runtime;
 using ILGPU.Runtime.Cuda;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Diagnostics;
 
 namespace MandelbrotSet
 {
@@ -11,110 +14,268 @@ namespace MandelbrotSet
     {
         private Bitmap mandelbrotBitmap;
         private const int MaxIterations = 1000;
-        private const float MinRe = -2.0f;
-        private const float MaxRe = 1.0f;
-        private const float MinIm = -1.2f;
-        private const float MaxIm = 1.2f;
+        private double MinRe = -2.0;
+        private double MaxRe = 1.0;
+        private double MinIm = -1.2;
+        private double MaxIm = 1.2;
+        private double zoomFactor = 1.0;
+        private PointF panOffset = PointF.Empty;
+        private Point lastMousePosition = Point.Empty;
+        private bool isPanning = false;
+        private Context context;
+        private Accelerator accelerator;
+        private Action<Index2D, ArrayView2D<uint, Stride2D.DenseX>, int, double, double, double, double> mandelbrotKernel;
+        private MemoryBuffer2D<uint, Stride2D.DenseX> colorBuffer;
 
         public Form1()
         {
             InitializeComponent();
+            this.MouseWheel += new MouseEventHandler(this.Form1_MouseWheel);
+            this.pictureBox1.MouseDown += new MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new MouseEventHandler(this.PictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new MouseEventHandler(this.PictureBox1_MouseUp);
+            this.ClientSize = new Size(1920, 1080);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+            InitializeGPU();
+        }
+
+        private void InitializeGPU()
+        {
+            context = Context.CreateDefault();
+            accelerator = context.GetCudaDevice(0).CreateAccelerator(context);
+            mandelbrotKernel = accelerator.LoadAutoGroupedStreamKernel<Index2D, ArrayView2D<uint, Stride2D.DenseX>, int, double, double, double, double>(MandelbrotKernel);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            mandelbrotBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            EnsureBufferSize();
             GenerateMandelbrotSet();
-            pictureBox1.Image = mandelbrotBitmap;
+        }
+
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            double zoomChange = 0.1 * (e.Delta > 0 ? 1 : -1);
+            PointF zoomCenter = pictureBox1.PointToClient(Cursor.Position);
+            ZoomMandelbrot(zoomChange, zoomCenter);
+        }
+
+        private void ZoomMandelbrot(double zoomChange, PointF zoomCenter)
+        {
+            double zoomCenterRe = MinRe + (MaxRe - MinRe) * zoomCenter.X / pictureBox1.Width;
+            double zoomCenterIm = MinIm + (MaxIm - MinIm) * zoomCenter.Y / pictureBox1.Height;
+
+            double newZoomFactor = zoomFactor * (1.0 + zoomChange);
+            double widthRe = (MaxRe - MinRe) / newZoomFactor;
+            double heightIm = (MaxIm - MinIm) / newZoomFactor;
+
+            MinRe = zoomCenterRe - widthRe * zoomCenter.X / pictureBox1.Width;
+            MaxRe = MinRe + widthRe;
+            MinIm = zoomCenterIm - heightIm * zoomCenter.Y / pictureBox1.Height;
+            MaxIm = MinIm + heightIm;
+
+            zoomFactor = newZoomFactor;
+
+            GenerateMandelbrotSet();
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isPanning = true;
+                lastMousePosition = e.Location;
+            }
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPanning)
+            {
+                panOffset.X += e.X - lastMousePosition.X;
+                panOffset.Y += e.Y - lastMousePosition.Y;
+                lastMousePosition = e.Location;
+                UpdateMandelbrotBounds();
+                GenerateMandelbrotSet();
+            }
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isPanning = false;
+            }
+        }
+
+        private void UpdateMandelbrotBounds()
+        {
+            double centerX = (MinRe + MaxRe) / 2.0;
+            double centerY = (MinIm + MaxIm) / 2.0;
+
+            double width = (MaxRe - MinRe);
+            double height = (MaxIm - MinIm);
+
+            MinRe = centerX - width / 2.0 + panOffset.X / pictureBox1.Width * width;
+            MaxRe = centerX + width / 2.0 + panOffset.X / pictureBox1.Width * width;
+            MinIm = centerY - height / 2.0 + panOffset.Y / pictureBox1.Height * height;
+            MaxIm = centerY + height / 2.0 + panOffset.Y / pictureBox1.Height * height;
+
+            panOffset = PointF.Empty;
+        }
+
+        private void EnsureBufferSize()
+        {
+            var width = pictureBox1.Width;
+            var height = pictureBox1.Height;
+
+            Debug.WriteLine($"EnsureBufferSize called with dimensions: {width}x{height}");
+
+            if (colorBuffer == null || colorBuffer.Extent.X != height || colorBuffer.Extent.Y != width)
+            {
+                colorBuffer?.Dispose();
+                // ILGPU için Index2D'yi height, width sırasıyla oluşturuyoruz
+                colorBuffer = accelerator.Allocate2DDenseX<uint>(new Index2D(height, width));
+                mandelbrotBitmap?.Dispose();
+                mandelbrotBitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+
+                Debug.WriteLine($"New colorBuffer allocated with dimensions: {colorBuffer.Extent.X}x{colorBuffer.Extent.Y}");
+                Debug.WriteLine($"New mandelbrotBitmap created with dimensions: {mandelbrotBitmap.Width}x{mandelbrotBitmap.Height}");
+            }
+            else
+            {
+                Debug.WriteLine("Existing buffers reused");
+            }
         }
 
         private void GenerateMandelbrotSet()
         {
-            using (var context = Context.CreateDefault())
-            using (var accelerator = context.GetCudaDevice(0).CreateAccelerator(context))
+            var width = pictureBox1.Width;
+            var height = pictureBox1.Height;
+
+            Debug.WriteLine($"GenerateMandelbrotSet called with dimensions: {width}x{height}");
+
+            EnsureBufferSize();
+
+            try
             {
-                var kernel = accelerator.LoadAutoGroupedStreamKernel<Index2D, ArrayView2D<int, Stride2D.DenseX>, int, int, float, float, float, float>(MandelbrotKernel);
+                // ILGPU için Index2D'yi height, width sırasıyla oluşturuyoruz
+                mandelbrotKernel(new Index2D(height, width), colorBuffer.View, MaxIterations, MinRe, MaxRe, MinIm, MaxIm);
 
-                var width = mandelbrotBitmap.Width;
-                var height = mandelbrotBitmap.Height;
+                accelerator.Synchronize();
 
-                using (var buffer = accelerator.Allocate2DDenseX<int>(new Index2D(width, height)))
+                Debug.WriteLine("GPU kernel execution completed");
+
+                var colorData = colorBuffer.GetAsArray2D();
+
+                Debug.WriteLine($"colorData retrieved. Dimensions: {colorData.GetLength(0)}x{colorData.GetLength(1)}");
+                Debug.WriteLine($"mandelbrotBitmap dimensions: {mandelbrotBitmap.Width}x{mandelbrotBitmap.Height}");
+
+                // Boyutları kontrol ederken sırayı değiştiriyoruz
+                if (colorData.GetLength(0) != height || colorData.GetLength(1) != width)
                 {
-                    kernel(buffer.Extent.ToIntIndex(), buffer.View, MaxIterations, width, MinRe, MaxRe, MinIm, MaxIm);
-                    accelerator.Synchronize();
+                    throw new InvalidOperationException($"Mismatch in dimensions. Expected: {width}x{height}, Got: {colorData.GetLength(1)}x{colorData.GetLength(0)}");
+                }
 
-                    var data = buffer.GetAsArray2D();
+                BitmapData bitmapData = mandelbrotBitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
 
-                    // Buffer boyutlarını kontrol et
-                    Console.WriteLine($"Buffer dimensions: {buffer.Extent.X}x{buffer.Extent.Y}");
-                    Console.WriteLine($"Data dimensions: {data.GetLength(0)}x{data.GetLength(1)}");
-                    Console.WriteLine($"Width: {width}, Height: {height}");
+                Debug.WriteLine("Bitmap locked for writing");
 
-                    for (int y = 0; y < height && y < data.GetLength(0); y++)
+                unsafe
+                {
+                    uint* pixelPtr = (uint*)bitmapData.Scan0;
+
+                    for (int y = 0; y < height; y++)
                     {
-                        for (int x = 0; x < width && x < data.GetLength(1); x++)
+                        for (int x = 0; x < width; x++)
                         {
-                            int iterations = data[y, x];
-                            Color color = GetColor(iterations);
-                            mandelbrotBitmap.SetPixel(x, y, color);
+                            // colorData'nın boyutlarını ters çeviriyoruz
+                            pixelPtr[y * width + x] = colorData[y, x];
                         }
                     }
                 }
+
+                Debug.WriteLine("Pixel data written to bitmap");
+
+                mandelbrotBitmap.UnlockBits(bitmapData);
+
+                Debug.WriteLine("Bitmap unlocked");
+
+                pictureBox1.Image = mandelbrotBitmap;
+                pictureBox1.Invalidate();
+
+                Debug.WriteLine("PictureBox updated");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Exception occurred: {ex.GetType().Name}");
+                Debug.WriteLine($"Exception message: {ex.Message}");
+                Debug.WriteLine($"Exception stack trace: {ex.StackTrace}");
+                MessageBox.Show($"An error occurred: {ex.Message}\n\nStack Trace: {ex.StackTrace}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        private static void MandelbrotKernel(Index2D index, ArrayView2D<int, Stride2D.DenseX> buffer, int maxIterations, int width, float minRe, float maxRe, float minIm, float maxIm)
-        {
-            float x0 = minRe + (maxRe - minRe) * index.X / (width - 1);
-            float y0 = minIm + (maxIm - minIm) * index.Y / (width - 1);
 
-            float x = 0.0f;
-            float y = 0.0f;
+
+        private static void MandelbrotKernel(Index2D index, ArrayView2D<uint, Stride2D.DenseX> colorBuffer, int maxIterations, double minRe, double maxRe, double minIm, double maxIm)
+        {
+            // index.X ve index.Y'yi ters çeviriyoruz
+            double x0 = minRe + (maxRe - minRe) * index.Y / (colorBuffer.Extent.Y - 1);
+            double y0 = minIm + (maxIm - minIm) * index.X / (colorBuffer.Extent.X - 1);
+
+            double x = 0.0;
+            double y = 0.0;
             int iteration = 0;
 
-            while (x * x + y * y <= 4.0f && iteration < maxIterations)
+            while (x * x + y * y <= 4.0 && iteration < maxIterations)
             {
-                float xtemp = x * x - y * y + x0;
-                y = 2.0f * x * y + y0;
+                double xtemp = x * x - y * y + x0;
+                y = 2.0 * x * y + y0;
                 x = xtemp;
                 iteration++;
             }
 
-            buffer[index] = iteration;
+            colorBuffer[index] = GetColor(iteration, maxIterations);
         }
 
-        private Color GetColor(int iteration)
+        private static uint GetColor(int iteration, int maxIterations)
         {
-            if (iteration == MaxIterations)
-                return Color.Black;
+            if (iteration == maxIterations)
+                return 0xFF000000; // Black
 
-            float hue = (float)iteration / MaxIterations;
+            double hue = (double)iteration / maxIterations;
             return ColorFromHSV(hue, 1, 1);
         }
 
-        private Color ColorFromHSV(float hue, float saturation, float value)
+        private static uint ColorFromHSV(double hue, double saturation, double value)
         {
-            int hi = Convert.ToInt32(Math.Floor(hue * 6)) % 6;
-            float f = hue * 6 - (float)Math.Floor(hue * 6);
+            int hi = (int)(hue * 6) % 6;
+            double f = hue * 6 - Math.Floor(hue * 6);
 
-            value = value * 255;
-            int v = Convert.ToInt32(value);
-            int p = Convert.ToInt32(value * (1 - saturation));
-            int q = Convert.ToInt32(value * (1 - f * saturation));
-            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+            byte v = (byte)(value * 255);
+            byte p = (byte)(v * (1 - saturation));
+            byte q = (byte)(v * (1 - f * saturation));
+            byte t = (byte)(v * (1 - (1 - f) * saturation));
 
-            if (hi == 0)
-                return Color.FromArgb(255, v, t, p);
-            else if (hi == 1)
-                return Color.FromArgb(255, q, v, p);
-            else if (hi == 2)
-                return Color.FromArgb(255, p, v, t);
-            else if (hi == 3)
-                return Color.FromArgb(255, p, q, v);
-            else if (hi == 4)
-                return Color.FromArgb(255, t, p, v);
-            else
-                return Color.FromArgb(255, v, p, q);
+            switch (hi)
+            {
+                case 0: return (uint)((0xFF << 24) | (v << 16) | (t << 8) | p);
+                case 1: return (uint)((0xFF << 24) | (q << 16) | (v << 8) | p);
+                case 2: return (uint)((0xFF << 24) | (p << 16) | (v << 8) | t);
+                case 3: return (uint)((0xFF << 24) | (p << 16) | (q << 8) | v);
+                case 4: return (uint)((0xFF << 24) | (t << 16) | (p << 8) | v);
+                default: return (uint)((0xFF << 24) | (v << 16) | (p << 8) | q);
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            colorBuffer?.Dispose();
+            mandelbrotBitmap?.Dispose();
+            accelerator?.Dispose();
+            context?.Dispose();
         }
     }
 }
